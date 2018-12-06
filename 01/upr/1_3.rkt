5ff4a5e4867a39703ddf694d0992e9ec25c5c41c
uldash <uldashevue@gmail>
2018-12-06 20:20:47 +0300

    delete 01

--- 01/upr/1_3.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-; возвращает сумму квадратов двух больших чисел
-(define (sqr-sum a b) (+ (* a a) (* b b)))
-
-(define (two-sqr-sum a b c)
-        (cond ((and (< a b) (< a c)) (sqr-sum b c))
-              ((and (< b a) (< b c)) (sqr-sum a c))
-              (else (sqr-sum a b))))
-(two-sqr-sum 1 3 4)
